4fa8799f4b0e8f55754d8c2a7cc479042cce32db
max <olgeer@163.com>
2022-01-21 09:39:57 +0800

    调整依赖包;

--- lib/define.dart
+++ lib/define.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 typedef eventCall = Function(dynamic value);
 typedef contextProc = Function(BuildContext context);
 typedef actionCall = Function();
+typedef voidProc = Function();
 
 extension MatchExtension on Schedule {
   bool match(DateTime now) {
